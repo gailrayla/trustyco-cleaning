354245e147892beb29b5fe9b9d1edd09ccd6d39c
Gail Rayla Emanuelle Parayno <paraynogail@gmail.com>
2025-09-25 10:13:54 +0800

    update config

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   emailjs: {
-    serviceId: 'service_ksvrfex',
+    serviceId: 'service_nhp8h9f',
     templateId: 'template_o5x8bcc',
     publicKey: 'l-M5KJzDninPeXPan',
   },
